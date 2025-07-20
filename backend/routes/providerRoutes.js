f6e6341553fd0fbb76eb8b6eee684a7f40f99de5
akhmedovkb <akhmedovkb@gmail.com>
2025-07-20 22:48:35 +0500

    Update providerRoutes.js

--- backend/routes/providerRoutes.js
+++ backend/routes/providerRoutes.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const { registerProvider } = require("../controllers/providerController");
+const {
+  registerProvider,
+  loginProvider
+} = require("../controllers/providerController");
 
+// POST /api/providers/register
 router.post("/register", registerProvider);
+
+// POST /api/providers/login
+router.post("/login", loginProvider);
 
 module.exports = router;
