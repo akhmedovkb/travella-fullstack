0390e7f97d55981f9cfd1eb5fd07f82415a85a57
akhmedovkb <akhmedovkb@gmail.com>
2025-07-20 15:51:03 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,16 @@
-
 const express = require("express");
 const cors = require("cors");
-const app = express();
-const providerRoutes = require("./routes/providerRoutes");
-const clientRoutes = require("./routes/clientRoutes");
+const dotenv = require("dotenv");
+dotenv.config();
 
-require("dotenv").config();
+const app = express();
 app.use(cors());
 app.use(express.json());
 
+const providerRoutes = require("./routes/providerRoutes");
 app.use("/api/providers", providerRoutes);
-app.use("/api/clients", clientRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Сервер запущен на порту ${PORT}`);
+});
