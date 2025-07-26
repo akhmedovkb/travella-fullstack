aea9fe3fc6bd452a8394bf445b8724623ee09d4b
akhmedovkb <akhmedovkb@gmail.com>
2025-07-26 20:10:26 +0500

    Update Dashboard.jsx

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -318,7 +318,7 @@ const getCategoryOptions = (type) => {
               marginHeight="0"
               marginWidth="0"
               className="rounded"
-              src={https://www.google.com/maps?q=${encodeURIComponent(profile.address)}&output=embed}
+              src={`https://www.google.com/maps?q=${encodeURIComponent(profile.address)}&output=embed}`
             ></iframe>
           </div>
         )}
