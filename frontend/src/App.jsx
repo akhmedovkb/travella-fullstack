e531690cf04f49925f3dfad855539fbff296a370
akhmedovkb <akhmedovkb@gmail.com>
2025-07-27 12:37:26 +0500

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,24 +5,27 @@ import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import PrivateRoute from "./pages/PrivateRoute";
 import Marketplace from "./pages/Marketplace";
+import LanguageSelector from "./components/LanguageSelector";
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route
-          path="/dashboard"
-          element={
-            <PrivateRoute>
-              <Dashboard />
-            </PrivateRoute>
-          }
-        />
-        <Route path="/marketplace" element={<Marketplace />} />
-
-      </Routes>
+      <div className="min-h-screen bg-gray-100 p-4">
+        <LanguageSelector />
+        <Routes>
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="/dashboard"
+            element={
+              <PrivateRoute>
+                <Dashboard />
+              </PrivateRoute>
+            }
+          />
+          <Route path="/marketplace" element={<Marketplace />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
