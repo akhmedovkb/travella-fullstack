16a06f68a2415906edec39e325ad0ab0c883c8f9
akhmedovkb <akhmedovkb@gmail.com>
2025-07-27 12:45:33 +0500

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,13 @@
+// frontend/src/main.jsx
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-import './i18n';
+import './i18n'; // Подключение конфигурации i18n
+
 import { I18nextProvider } from 'react-i18next';
-import i18n from './i18n';
+import i18n from './i18n'; // Импорт инстанса i18n
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
