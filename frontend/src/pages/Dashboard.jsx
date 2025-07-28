49ea5f3ec3a75df6e279405c74549e727dc89141
akhmedovkb <akhmedovkb@gmail.com>
2025-07-28 12:23:31 +0500

    Update Dashboard.jsx

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -803,6 +803,7 @@ const getCategoryOptions = (type) => {
     value={selectedCountry}
     onChange={(value) => setSelectedCountry(value)}
     placeholder={t("direction_country")}
+    noOptionsMessage={() => t("country_not_chosen")}
     className="w-1/3"
   />
   
@@ -815,6 +816,7 @@ const getCategoryOptions = (type) => {
     setDetails((prev) => ({ ...prev, directionFrom: selected?.value }));
   }}
   placeholder={t("direction_from")}
+  noOptionsMessage={() => t("direction_from_not_chosen")}
   className="w-1/3"
 />
 
@@ -822,6 +824,7 @@ const getCategoryOptions = (type) => {
 <Select
   options={cityOptionsTo}
   placeholder={t("direction_to")}
+  noOptionsMessage={() => t("direction_to_not_chosen")}
   onChange={(value) =>
     setDetails({ ...details, directionTo: value?.value })
   }
