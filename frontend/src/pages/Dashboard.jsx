9325a764a443daad5595371132b20baf1816c6ce
akhmedovkb <akhmedovkb@gmail.com>
2025-07-30 23:48:48 +0500

    Update Dashboard.jsx

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1419,10 +1419,10 @@ const getCategoryOptions = (type) => {
         </button>
       </div>
     </>
-     ) 
+     ) : null}
     )
   )
- ): null}
+ ) : null}
 
       <div className="bg-yellow-100 border-l-4 border-yellow-500 text-yellow-800 p-4 rounded mb-4">
         {t("new_service_tip")}
