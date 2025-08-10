6558e9387ae515b4fdd349005aed4386a90540e4
akhmedovkb <akhmedovkb@gmail.com>
2025-08-10 23:10:33 +0500

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,15 +3,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import "./i18n"; // 💥 обязательно ДО App
+import "./i18n";
 
 import { I18nextProvider } from "react-i18next";
 import i18n from "./i18n";
+
+// 👇 добавь это
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <I18nextProvider i18n={i18n}>
       <App />
+      {/* единственный контейнер тостов на всём приложении */}
+      <ToastContainer position="top-right" autoClose={3000} newestOnTop />
     </I18nextProvider>
   </React.StrictMode>
 );
