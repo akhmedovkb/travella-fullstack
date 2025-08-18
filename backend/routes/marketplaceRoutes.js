4971f1a0e4591b5ce9555bac67eaa3f6960d27b2
akhmedovkb <akhmedovkb@gmail.com>
2025-08-18 08:50:35 +0500

    Update marketplaceRoutes.js

--- backend/routes/marketplaceRoutes.js
+++ backend/routes/marketplaceRoutes.js
@@ -1,9 +1,8 @@
+// backend/routes/marketplaceRoutes.js
 const express = require("express");
 const router = express.Router();
 const ctrl = require("../controllers/marketplaceController");
 
-// поддерживаем оба метода
-router.get("/search", ctrl.search);
 router.post("/search", ctrl.search);
 
 module.exports = router;
