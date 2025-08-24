51759be9b889ce5f518697031119fa9139294b9d
akhmedovkb <akhmedovkb@gmail.com>
2025-08-24 13:20:59 +0500

    Update profileRoutes.js

--- backend/routes/profileRoutes.js
+++ backend/routes/profileRoutes.js
@@ -1,1 +1,10 @@
+const express = require("express");
+const router = express.Router();
+
+const { getClientPublicProfile } = require("../controllers/profileController");
+
+// Публичный (без авторизации): профиль клиента по id
+router.get("/client/:id", getClientPublicProfile);
+
+module.exports = router;
 
