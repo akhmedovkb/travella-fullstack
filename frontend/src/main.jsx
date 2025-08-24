3ffdaf21cfeef770b1a061048610deea2c103f30
akhmedovkb <akhmedovkb@gmail.com>
2025-08-24 17:35:03 +0500

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,9 +5,7 @@ import "./index.css";
 import "./i18n";
 import { I18nextProvider } from "react-i18next";
 import i18n from "./i18n";
-
-// берем Toaster из нашей обёртки
-import { Toaster } from "./ui/toast";
+import { Toaster } from "react-hot-toast";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
