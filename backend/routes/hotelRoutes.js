89c43740045d0e004a8f79b398a1e3d128c018de
akhmedovkb <akhmedovkb@gmail.com>
2025-09-14 23:01:06 +0500

    Update hotelRoutes.js

--- backend/routes/hotelRoutes.js
+++ backend/routes/hotelRoutes.js
@@ -1,8 +1,20 @@
 // backend/routes/hotelRoutes.js
 const express = require("express");
 const router = express.Router();
-const { searchHotels } = require("../controllers/hotelController");
 
+const {
+  searchHotels,
+  createHotel,
+  getHotel,
+  listHotels,
+} = require("../controllers/hotelController");
+
+// поиск для автодополнения
 router.get("/search", searchHotels);
+
+// CRUD-часть
+router.post("/", createHotel);
+router.get("/:id", getHotel);
+router.get("/", listHotels);
 
 module.exports = router;
