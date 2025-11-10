0cffcffa56d787eb2dd55ba81a05b574d3e57030
akhmedovkb <akhmedovkb@gmail.com>
2025-11-10 22:43:53 +0500

    Update Clinics.jsx

--- frontend/src/pages/landing/Clinics.jsx
+++ frontend/src/pages/landing/Clinics.jsx
@@ -1,12 +1,13 @@
 //frontend/src/pages/landing/Clinics.jsx
 import { useTranslation } from "react-i18next";
 
-export default function Checkup() {
+export default function Clinics() {
   const { t } = useTranslation();
   return (
     <main className="py-2">
-      <h1 className="text-3xl md:text-5xl font-bold">{t("landing.checkup.h1")}</h1>
-      <p className="mt-3 text-lg">{t("landing.checkup.sub")}</p>
+      <h1 className="text-3xl md:text-5xl font-bold">{t("landing.clinics.h1")}</h1>
+      <p className="mt-3 text-lg">{t("landing.clinics.sub")}</p>
+      {/* только инфо */}
     </main>
   );
 }
