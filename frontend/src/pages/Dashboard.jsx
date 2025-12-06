e29c69b67f8b32244b6d6ad94d4fb03670f08e37
akhmedovkb <akhmedovkb@gmail.com>
2025-12-06 17:37:30 +0500

    Update Dashboard.jsx

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1569,7 +1569,7 @@ useEffect(() => {
         </div>
       )}
       <div className="flex flex-col md:flex-row gap-4 md:gap-6 p-4 md:p-6 bg-gray-50 min-h-[calc(var(--vh,1vh)*100)] pb-[env(safe-area-inset-bottom)]">
-        { блок: услуги + входящие/брони */}
+        {/* блок: услуги + входящие/брони */}
         <div className="w-full md:w-1/2 bg-white p-6 rounded-xl shadow-md">
             {/* === Отель: прайс и карточка === */}
           {profile?.type === "hotel" && (
