ab0596a09fe500a464eb630369c3f27289e02d66
akhmedovkb <akhmedovkb@gmail.com>
2026-02-03 20:39:24 +0500

    Create adminDonasSalesRoutes.js

---
+++ backend/routes/adminDonasSalesRoutes.js
@@ -0,0 +1,12 @@
+// backend/routes/adminDonasSalesRoutes.js
+const express = require("express");
+const router = express.Router();
+
+const ctrl = require("../controllers/donasSalesController");
+
+router.get("/sales", ctrl.getSales);
+router.post("/sales", ctrl.addSale);
+router.put("/sales/:id", ctrl.updateSale);
+router.delete("/sales/:id", ctrl.deleteSale);
+
+module.exports = router;
