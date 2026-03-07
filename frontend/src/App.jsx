81167edc3a93540f05e86bf770c65fba8e32f090
akhmedovkb <akhmedovkb@gmail.com>
2026-03-07 19:07:27 +0500

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,5 @@
+//frontend/src/App.jsx
+
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { ToastMount } from "./shared/toast";
