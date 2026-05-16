4c30b3ab321795e2664769ecf9824ffb3ca46f68
akhmedovkb <akhmedovkb@gmail.com>
2026-05-16 10:39:00 +0500

    Update marketplaceRoutes.js

--- backend/routes/marketplaceRoutes.js
+++ backend/routes/marketplaceRoutes.js
@@ -3,6 +3,7 @@ const express = require("express");
 const router = express.Router();
 const ctrl = require("../controllers/marketplaceController");
 
+router.get("/services/:id", ctrl.getById);
 router.post("/search", ctrl.search);
 router.get("/search", ctrl.search);
 router.get("/suggest", ctrl.suggest);
