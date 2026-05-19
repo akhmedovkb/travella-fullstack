36863158d652e72d5ebe7579a2bfbea3785ab9da
akhmedovkb <akhmedovkb@gmail.com>
2026-05-19 09:55:10 +0500

    Update hotelInspectionRoutes.js

--- backend/routes/hotelInspectionRoutes.js
+++ backend/routes/hotelInspectionRoutes.js
@@ -1,9 +1,19 @@
 //backend/routes/hotelInspectionRoutes.js
 
 const router = require("express").Router();
-const { likeInspection } = require("../controllers/hotelInspectionController");
+const authenticateToken = require("../middleware/authenticateToken");
+const { likeInspection } = require("../controllers/hotelsController");
 
-// Лайк конкретной инспекции
-router.post("/:inspectionId/like", likeInspection);
+function tryAuth(req, res, next) {
+  const hdr = req.headers?.authorization || "";
+  if (!hdr) return next();
+  authenticateToken(req, res, () => next());
+}
+
+// Лайк конкретной инспекции. Поддерживает старый путь frontend: /api/hotel-inspections/:inspectionId/like
+router.post("/:inspectionId/like", tryAuth, (req, res, next) => {
+  req.params.id = req.params.inspectionId;
+  return likeInspection(req, res, next);
+});
 
 module.exports = router;
