81be83f65b9a3ff79af620bf49b03eac0e2c4417
akhmedovkb <akhmedovkb@gmail.com>
2026-06-02 14:07:11 +0500

    Update adminPaymePaymentsRoutes.js

--- backend/routes/adminPaymePaymentsRoutes.js
+++ backend/routes/adminPaymePaymentsRoutes.js
@@ -4,10 +4,16 @@ const express = require("express");
 const authenticateToken = require("../middleware/authenticateToken");
 const requireAdmin = require("../middleware/requireAdmin");
 
-const { adminPaymePayments } = require("../controllers/adminPaymePaymentsController");
+const {
+  adminPaymePayments,
+  expireAbandonedPaymePayments,
+  sendAbandonedPaymeReminders,
+} = require("../controllers/adminPaymePaymentsController");
 
 const router = express.Router();
 
 router.get("/payments", authenticateToken, requireAdmin, adminPaymePayments);
+router.post("/payments/expire", authenticateToken, requireAdmin, expireAbandonedPaymePayments);
+router.post("/payments/reminders", authenticateToken, requireAdmin, sendAbandonedPaymeReminders);
 
 module.exports = router;
