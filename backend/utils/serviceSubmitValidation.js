c1c7604e5ab1678ca8e714aa49d7ef4619d2c36a
akhmedovkb <akhmedovkb@gmail.com>
2026-06-27 17:55:18 +0500

    Update serviceSubmitValidation.js

--- backend/utils/serviceSubmitValidation.js
+++ backend/utils/serviceSubmitValidation.js
@@ -1,35 +1,14 @@
 // backend/utils/serviceSubmitValidation.js
 
-const { normalizeCategory, isProofRequiredCategory } = require("./serviceCategories");
 const {
-  getSubmitChecks,
+  normalizeDetails,
   getProofImages,
   getImages,
+  getSubmitBlockers,
 } = require("./serviceFieldMatrix");
-const { normalizeDetails } = require("./serviceDisplay");
 
 function buildSubmitValidationBlockers(service = {}) {
-  const category = normalizeCategory(service.category);
-  const d = normalizeDetails(service.details);
-  const blockers = [];
-
-  for (const check of getSubmitChecks(service)) {
-    if (!check.ok) {
-      blockers.push({
-        code: check.code || String(check.key || "FIELD_REQUIRED").toUpperCase(),
-        label: check.label || "Заполните поле",
-      });
-    }
-  }
-
-  if (isProofRequiredCategory(category) && getProofImages(d).length <= 0) {
-    blockers.push({
-      code: "PROOF_IMAGES_REQUIRED",
-      label: "Добавьте proof: скрин/ваучер/билет/подтверждение",
-    });
-  }
-
-  return blockers;
+  return getSubmitBlockers(service);
 }
 
 function assertServiceSubmittable(service) {
@@ -51,4 +30,5 @@ module.exports = {
   assertServiceSubmittable,
   getProofImages,
   getImages,
+  normalizeDetails,
 };
